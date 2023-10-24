264bfe11c27d25d3b8e5003980018dab027dd7ae
olusiekwin <olusiekwin@gmail.com>
2023-10-24 11:39:54 +0300

    Fix it

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Module Docs"""
+
+
+class Square():
+    """ Class Docs"""
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        """ Function Docs """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area Docs"""
+        return self.width * self.height
+
+    def permiter_of_my_square(self):
+        """ Perimeter Docs """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """ STR DOCS """
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    """ Final DOcs """
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
